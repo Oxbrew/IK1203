5dbbcc14accce998302d577ce2714a7e0e37b13f
Oxbrew <13368707+Oxbrew@users.noreply.github.com>
2020-02-10 16:33:52 +0100

    Task 1

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -3,11 +3,46 @@ import java.net.*;
 import java.io.*;
 
 public class TCPClient {
-    
+
+	private static int BUFFERSIZE = (1024 * 8);
+
     public static String askServer(String hostname, int port, String ToServer) throws  IOException {
+		if(ToServer == null){
+			return askServer(hostname, port);
+		}
+
+		byte [] clientBuffer = new byte[BUFFERSIZE];
+		byte [] serverBuffer = new byte[BUFFERSIZE];
+		clientBuffer = ToServer.getBytes();
+		Socket socket = new Socket(hostname, port);
+
+
+		socket.getOutputStream().write(clientBuffer);
+		socket.getOutputStream().write('\n');
+
+		int serverBufferLength = socket.getInputStream().read(serverBuffer);
+
+		String string = new String(serverBuffer, 0, serverBufferLength);
+		System.out.print('\n');
+
+		socket.close();
+
+		return string;
+
+
     }
 
     public static String askServer(String hostname, int port) throws  IOException {
+
+		byte [] serverBuffer = new byte[BUFFERSIZE];
+		Socket socket = new Socket(hostname, port);
+
+		int serverBufferLength = socket.getInputStream().read(serverBuffer);
+		String string = new String(serverBuffer, 0, serverBufferLength);
+		System.out.print('\n');
+
+		socket.close();
+
+		return string;
     }
 }
-
